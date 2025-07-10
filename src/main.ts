1b811c9d8c8f489e2248ed77d84608b6ce960d43
Snoopy <litlong99@gmail.com>
2025-07-10 14:35:16 +0700

    feat: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { configSwagger } from './configs/api-docs.config';
-import { All, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './exceptions/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+  const origin = configService.get<string>('CORS_ORIGIN') || '*';
   configSwagger(app, configService);
 
   app.useGlobalPipes(
@@ -16,10 +18,13 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.enableCors({
+    origin: origin.includes(',')
+      ? origin.split(',').map((item) => item.trim())
+      : origin,
+  });
 
-  const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
 }
 bootstrap();
